6c88a29ee7da79ed60583304885d41f396f5e127
Zach Helm <zac1790@gmail.com>
2021-04-08 07:59:38 -0400

    Stairway to Heaven in halftones, A5 root
    Press button to play UI sound and increment note in song

--- Assets/MusicalUI/MusicalSource.cs
+++ Assets/MusicalUI/MusicalSource.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicalSource : MonoBehaviour
+namespace MusicalUI
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [RequireComponent(typeof(AudioSource))]
+    public class MusicalSource : MonoBehaviour
     {
-        
+        public AudioClip audioClip;
+        private AudioSource audioSource;
+
+        public int detectedPitch;
+
+        [SerializeField]
+        private int[] halfstepsSong;
+        private int currentNote = 0;
+        [SerializeField]
+        private int offsetHalfstep = 0;
+
+        private float a = 1.059463094359f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+
+            detectedPitch = audioClip.frequency;
+            
+        }
+
+        public void PlayMusicalSound()
+        {
+            audioSource.pitch = Mathf.Pow(a, halfstepsSong[currentNote] + offsetHalfstep);
+            audioSource.PlayOneShot(audioClip);
+
+            currentNote++;
+            if (currentNote >= halfstepsSong.Length) currentNote = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
